3062a96639f15a5657faccbd3465ebf9f52324da
NoTimeForHero <notimeforhero@yandex.ru>
2017-10-27 02:54:22 +0300

    Полоса прокрутки и фикс выхода кнопки закрытия за пределы экрана

--- IconExtractor/FormViewIcon.Designer.cs
+++ IconExtractor/FormViewIcon.Designer.cs
@@ -34,6 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
             this.ClientSize = new System.Drawing.Size(268, 228);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
@@ -42,6 +43,7 @@
             this.ShowInTaskbar = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
             this.Text = "FormViewIcon";
+            this.Load += new System.EventHandler(this.FormViewIcon_Load);
             this.ResumeLayout(false);
 
         }
