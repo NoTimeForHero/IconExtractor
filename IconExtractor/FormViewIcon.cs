b5dfb4e59401d5c09b02a2b4eb61e67f31002213
Artyom Verchenko <artyomskynet@gmail.com>
2017-10-30 19:19:16 +0300

    Нормальное сохранение иконок всеми способами

--- IconExtractor/FormViewIcon.cs
+++ IconExtractor/FormViewIcon.cs
@@ -11,14 +11,29 @@ namespace IconExtractor
 {
     public partial class FormViewIcon : Form
     {
-        public FormViewIcon()
+        public int icon_id;
+        private FormIcon formIcon;
+
+        public FormViewIcon(FormIcon formIcon)
         {
+            this.formIcon = formIcon;
             InitializeComponent();
         }
 
         private void FormViewIcon_Load(object sender, EventArgs e)
         {
+            // Nothing to do here
+        }
 
+        public void showContextMenu(int icon_id)
+        {
+            this.icon_id = icon_id;
+            contextMenuStrip1.Show(Cursor.Position);
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            formIcon.SaveIconById(this, icon_id);
         }
     }
 }
